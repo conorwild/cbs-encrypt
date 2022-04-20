7a036f9befb11aabdb81ec86797e2d0828a4f3f2
Conor Wild <conor.wild@cambridgebrainsciences.com>
2022-04-20 15:14:36 -0400

    Added ability to do multiple files at once, space separated on cmd line

--- cbs_encrypt/file_encrypt.py
+++ cbs_encrypt/file_encrypt.py
@@ -8,17 +8,17 @@ def main():
 
     cipher = Fernet(key)
 
-    filename = sys.argv[1]
-    print(f"Encrypting: {filename} ...")
+    for filename in sys.argv[1:]:
+        print(f"Encrypting: {filename} ...")
 
-    with open(filename, 'rb') as rf:
-        e_file = rf.read()
-    encrypted_data = cipher.encrypt(e_file)
+        with open(filename, 'rb') as rf:
+            e_file = rf.read()
+        encrypted_data = cipher.encrypt(e_file)
 
-    path, file = os.path.split(filename)
-    e_filename = os.path.join(path, f"encrypted_{file}")
-    with open(e_filename, 'wb') as wf:
-        wf.write(encrypted_data)
+        path, file = os.path.split(filename)
+        e_filename = os.path.join(path, f"encrypted_{file}")
+        with open(e_filename, 'wb') as wf:
+            wf.write(encrypted_data)
 
     print('DONE')
 
