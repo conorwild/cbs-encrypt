29952a3105da96a7e9319aa0c554a141f68dbf88
Conor Wild <conor.wild@cambridgebrainsciences.com>
2022-06-15 12:16:52 -0400

    Added Crypt class that contains all the encrypt/decrypt functions, updated entrypoints

--- cbsencrypt/__init__.py
+++ cbsencrypt/__init__.py
@@ -1,3 +1,3 @@
 from .cbsencrypt import (
-    encrypt_files, decrypt_files, decrypt_file_to_bytestream
+    Crypt,
 )
--- cbsencrypt/cbsencrypt.py
+++ cbsencrypt/cbsencrypt.py
@@ -8,70 +8,86 @@ from cryptography.fernet import Fernet
 from io import BytesIO
 
 
-def ask_for_key():
-    return input("Enter your key: ")
+class Crypt():
 
+    @classmethod
+    def request_key(cls):
+        return input("Enter your key: ")
 
-def encrypt_files(files, output_directory=None):
-    cipher = Fernet(ask_for_key())
+    def __init__(self, key=None):
+        if key is None:
+            key = self.request_key()
+        self._cipher = Fernet(key)
 
-    for filename in files:
-        print(f"Encrypting: {filename} ...")
+    def encrypt(self, contents):
+        return self._cipher.encrypt(contents)
 
-        with open(filename, 'rb') as rf:
+    def decrypt(self, contents):
+        return self._cipher.decrypt(contents)
+
+    def encrypt_str(self, string_contents):
+        contents_bytes = str.encode(string_contents, 'utf-8')
+        encrypted_bytes = self.encrypt(contents_bytes)
+        return encrypted_bytes.decode('utf-8')
+
+    def decrypt_str(self, encrypted_string):
+        encrypted_bytes = str.encode(encrypted_string, 'utf-8')
+        decrypted_bytes = self.decrypt(encrypted_bytes)
+        return decrypted_bytes.decode('utf-8')
+
+    def encrypt_files(self, files, output_directory=None):
+        files = self.check_files(files)
+        for filename in files:
+            print(f"Encrypting: {filename} ...")
+
+            with open(filename, 'rb') as rf:
+                e_file = rf.read()
+
+            encrypted_data = self.encrypt(e_file)
+
+            fp = Path(filename)
+            if output_directory is None:
+                output_directory = fp.parent
+            Path(output_directory).mkdir(parents=True, exist_ok=True)
+
+            e_filename = path.join(output_directory, fp.name+".crypt")
+            with open(e_filename, 'wb') as wf:
+                wf.write(encrypted_data)
+
+    def decrypt_files(self, files, output_directory=None):
+        files = self.check_files(files)
+        for filename in files:
+            print(f"Decrypting: {filename} ...")
+
+            with open(filename, 'rb') as rf:
+                e_file = rf.read()
+
+            decrypted_data = self.decrypt(e_file)
+
+            fp = Path(filename)
+            if output_directory is None:
+                output_directory = fp.parent
+            Path(output_directory).mkdir(parents=True, exist_ok=True)
+
+            d_filename = path.join(output_directory, fp.name[0:-6])
+            with open(d_filename, 'wb') as wf:
+                wf.write(decrypted_data)
+
+    def decrypt_file_to_bytestream(self, filename, key=None):
+        with open(filename, 'rb') as rf:    
             e_file = rf.read()
+        decrypted_data = self.decrypt(e_file)
+        return BytesIO(decrypted_data)
 
-        encrypted_data = cipher.encrypt(e_file)
-
-        fp = Path(filename)
-        if output_directory is None:
-            output_directory = fp.parent
-        Path(output_directory).mkdir(parents=True, exist_ok=True)
-
-        e_filename = path.join(output_directory, fp.name+".crypt")
-        with open(e_filename, 'wb') as wf:
-            wf.write(encrypted_data)
-
-
-def decrypt_files(files, output_directory=None):
-    cipher = Fernet(ask_for_key())
-
-    for filename in files:
-        print(f"Decrypting: {filename} ...")
-
-        with open(filename, 'rb') as rf:
-            e_file = rf.read()
-
-        decrypted_data = cipher.decrypt(e_file)
-
-        fp = Path(filename)
-        if output_directory is None:
-            output_directory = fp.parent
-        Path(output_directory).mkdir(parents=True, exist_ok=True)
-
-        d_filename = path.join(output_directory, fp.name[0:-6])
-        with open(d_filename, 'wb') as wf:
-            wf.write(decrypted_data)
-
-
-def decrypt_file_to_bytestream(filename, key=None):
-    if key is None:
-        key = ask_for_key()
-    cipher = Fernet(key)
-    with open(filename, 'rb') as rf:    
-        e_file = rf.read()
-    decrypted_data = cipher.decrypt(e_file)
-    return BytesIO(decrypted_data)
-
-
-def check_files(files):
-    if len(files) == 0:
-        raise ValueError("No filenames provided.")
-    for file in files:
-        if not path.exists(file):
-            raise OSError(f"{file} does not exist.")
-            
-    return files
+    @classmethod
+    def check_files(cls, files):
+        if len(files) == 0:
+            raise ValueError("No filenames provided.")
+        for file in files:
+            if not path.exists(file):
+                raise OSError(f"{file} does not exist.")
+                
+        return files
 
 
 def decrypt_files_cmdline():
@@ -85,12 +101,12 @@ def decrypt_files_cmdline():
         help='A file (including path) to encrypt.'
     )
     args = vars(parser.parse_args())
-    decrypt_files(check_files(args['files']), args['output_directory'])
+
+    Crypt().decrypt_files(args['files'], args['output_directory'])
     print('DONE')
 
 
 def encrypt_files_cmdline():
-
     parser = argparse.ArgumentParser(description='Encrypt some files.')
     parser.add_argument(
         '-o', '--output-directory', type=str,
@@ -102,9 +118,9 @@ def encrypt_files_cmdline():
     )
     args = vars(parser.parse_args())
 
-    encrypt_files(check_files(args['files']), args['output_directory'])
+    Crypt().encrypt_files(args['files'], args['output_directory'])
     print('DONE')
 
 
 if __name__ == '__main__':
-    decrypt_files_cmdline()  # pylint: disable=no-value-for-parameter
+    encrypt_files_cmdline()  # pylint: disable=no-value-for-parameter
