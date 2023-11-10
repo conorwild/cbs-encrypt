042b016666d76e1a623ca626c76a54a115d04a43
Conor Wild <conor.wild@cambridgebrainsciences.com>
2023-11-10 11:51:12 -0500

    Fix for TypeError with new cryptography; linting fixes

--- cbsencrypt/generate_passkey.py
+++ cbsencrypt/generate_passkey.py
@@ -3,23 +3,25 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
+
 def main():
     password_str = input("Enter your password: ")
-    password = password_str.encode('utf-8')
+    password = password_str.encode("utf-8")
 
     my_salt = input("Enter your salt: ")
     assert len(my_salt) == 32
 
     kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256,
+        algorithm=hashes.SHA256(),
         length=32,
-        salt=my_salt.encode('utf-8'),
+        salt=my_salt.encode("utf-8"),
         iterations=100000,
-        backend=default_backend()
+        backend=default_backend(),
     )
 
     key = base64.urlsafe_b64encode(kdf.derive(password))
     print(key.decode())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()  # pylint: disable=no-value-for-parameter
